ea32bcb8f812aab99c76aa6dab67a3d96252b02f
Christian Friedow <christian@friedow.com>
2021-09-24 17:09:27 +0200

    [WIP] verify bearer token

--- src/routers/Authentication.ts
+++ src/routers/Authentication.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router: express.Router = express.Router();
 
 router.post("/", async (req, res) => {
-  
+  res.status(200).send();
 });
 
 export default router;
