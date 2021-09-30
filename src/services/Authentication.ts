ac9d5a2f9b8f06939928ce540e84dd3fd9c9ee3b
Christian Friedow <christian@friedow.com>
2021-09-30 09:12:08 +0200

    Use jsonwebtoken library instead of keycloak-backend

--- src/services/Authentication.ts
+++ src/services/Authentication.ts
@@ -1,14 +1,10 @@
 import express from "express";
 import AuthenticationError from "../utils/AuthenticationError";
 
-import keycloak from "keycloak-backend";
+import jwt from "jsonwebtoken";
 import CertficateModel from "@/models/Certificate";
 
 export default class AuthenticationService {
-  private static get keycloakInstance() {
-    return keycloak({});
-  }
-
   public static async verify(req: express.Request): Promise<void> {
     if (!req.headers.authorization)
       throw new AuthenticationError("Authorization header is missing.");
@@ -30,10 +26,19 @@ export default class AuthenticationService {
 
   private static async isValid(accessToken: string, certificate: string): Promise<boolean> {
     try {
-      await this.keycloakInstance.jwt.verifyOffline(accessToken, certificate);
+      await this.verifyJwt(accessToken, certificate);
       return true;
     } catch (e) {
       return false;
     }
+  }
+
+  private static verifyJwt(accessToken: string, cert: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      jwt.verify(accessToken, cert, {}, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
   }
 }
